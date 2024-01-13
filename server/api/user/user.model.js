79943137fb2eed672d0c093db171e9a405c7d4f0
Manish <manishjad2810@gmail.com>
2024-01-13 20:26:41 +0530

    Third Commit

---
+++ server/api/user/user.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    lastname: String,
+    firstname: String,
+});
+
+const model = mongoose.model('User', userSchema);
+
+module.exports = model;
