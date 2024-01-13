5cdc906e1f5cf612e3790fb1943926a16cf96c22
Manish <manishjad2810@gmail.com>
2024-01-13 17:28:18 +0530

    Second Commit

--- server/app.js
+++ server/app.js
@@ -1,12 +1,22 @@
 var express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB).then(()=>{
+  console.log('Database connection established');
+}).catch((error) => {
+  console.log(error);
+  console.log('Error Connecting to Mongo')
+})
 var usersRouter = require('./api/user/user.route.');
 
 var app = express();
 
 app.use((req , res , next) => {
   res.header('Access-Control-Allow-Origin','*');
-  res.header('Access-Control-Allow-Methods','*');  
-})
+  res.header('Access-Control-Allow-Methods','*');
+  next();  
+});
 
 app.use(express.json());
 app.use('/users', usersRouter);
